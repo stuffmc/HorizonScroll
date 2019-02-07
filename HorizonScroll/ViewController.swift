d853a4d8050d53d53c05207e5210762b583f659a
StuFF mc <m+git@carrascomolina.com>
2019-02-07 13:12:26 +0100

    Initial Commit

---
+++ HorizonScroll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HorizonScroll
+//
+//  Created by StuFF mc on 07.02.19.
+//  Copyright © 2019 StuFF mc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
